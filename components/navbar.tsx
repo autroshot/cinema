90ffa871979f448fbb8465c2919ee817be5ae7be
autroshot <autroshot@gamil.com>
2022-07-30 23:45:35 +0900

    네비바에 로고 추가

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,21 @@
+import Image from 'next/image';
+import logo from '../public/logo.png';
 import { Container, Navbar as BootstrapNavBar } from 'react-bootstrap';
 
 export default function Navbar() {
   return (
     <Container>
       <BootstrapNavBar expand="lg" bg="light">
-        <Container>navbar</Container>
+        <Container>
+          <BootstrapNavBar.Brand className="align-middle">
+            <Image src={logo} alt="시네마 로고" />
+            CINEMA
+          </BootstrapNavBar.Brand>
+          <span className="justify-content-end">
+            <span>회원 가입</span>
+            <span>로그인</span>
+          </span>
+        </Container>
       </BootstrapNavBar>
     </Container>
   );

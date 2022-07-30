80301780573c258f6d2907ed06005e53a7711fbc
autroshot <autroshot@gamil.com>
2022-07-30 20:23:37 +0900

    globals CSS에 부트스트랩 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Button } from 'react-bootstrap';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       contents
+      <Button variant="secondary">버튼</Button>
     </div>
   );
 }

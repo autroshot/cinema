246e7904f7da11eab276c6a754207be40ded3a6a
autroshot <autroshot@gamil.com>
2022-07-30 19:54:37 +0900

    App 컴포넌트에 Layout 컴포넌트 추가

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import { ScriptProps } from 'next/script';
 
 export default function Layout({ children }: ScriptProps) {
-  return <main>{children}</main>;
+  return <main>layout{children}</main>;
 }

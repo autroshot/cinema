5297c4357f67e13a278b4593eb65c2e070aa3db1
autroshot <autroshot@gamil.com>
2022-07-31 00:18:56 +0900

    Navbar CSS 생성. 로고 텍스트에 CSS 적용

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
 import logo from '../public/logo.png';
 import { Container, Navbar as BootstrapNavBar } from 'react-bootstrap';
+import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
   return (
     <Container>
       <BootstrapNavBar expand="lg" bg="light">
         <Container>
-          <BootstrapNavBar.Brand className="align-middle">
-            <Image src={logo} alt="시네마 로고" />
-            CINEMA
+          <BootstrapNavBar.Brand>
+            <Image src={logo} alt="시네마 로고" width={40} height={40} />
+            <span className={styles.logoText}>CINEMA</span>
           </BootstrapNavBar.Brand>
           <span className="justify-content-end">
             <span>회원 가입</span>

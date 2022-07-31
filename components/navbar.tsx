c615f2c360c23b3217238b96ae0e2b3d858e1c22
autroshot <autroshot@gamil.com>
2022-07-31 14:00:53 +0900

    네비바 태그 부트스트랩으로 변경

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import logo from '../public/logo.png';
-import { Container, Navbar as BootstrapNavBar } from 'react-bootstrap';
+import { Container, Nav, Navbar as BootstrapNavBar } from 'react-bootstrap';
 import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
@@ -12,15 +12,16 @@ export default function Navbar() {
             <Image src={logo} alt="시네마 로고" width={40} height={40} />
             <span className={styles.logoText}>CINEMA</span>
           </BootstrapNavBar.Brand>
-          <div className="flex-column me-auto">
-            <ul className="navbar-nav flex-row">
-              <li className="nav-item">회원가입</li>
-              <li className="nav-item">로그인</li>
-            </ul>
-            <ul className="navbar-nav flex-row">
-              <li className="nav-item">회원가입</li>
-              <li className="nav-item">로그인</li>
-            </ul>
+          <div className="me-auto flex-column">
+            <Nav className="flex-row">
+              <Nav.Link>회원가입</Nav.Link>
+              <Nav.Link>로그인</Nav.Link>
+            </Nav>
+            <Nav className="flex-row">
+              <Nav.Link>예매</Nav.Link>
+              <Nav.Link>영화</Nav.Link>
+              <Nav.Link>영화관</Nav.Link>
+            </Nav>
           </div>
         </Container>
       </BootstrapNavBar>

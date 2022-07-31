682d13113deacea6d1c6ca2770f6936389a08012
autroshot <autroshot@gamil.com>
2022-07-31 17:00:54 +0900

    푸터에 깃허브 링크 추가

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,19 @@
 import Image from 'next/image';
 import logo from '../public/logoWithTextGray.png';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import styles from '../styles/Footer.module.css';
 
 export default function Footer() {
   return (
     <Container>
-      <div>
-        <Image src={logo} alt="시네마 로고" />
-      </div>
+      <footer>
+        <div>
+          <Image src={logo} alt="시네마 로고" />
+        </div>
+        <div>
+          <a href="https://github.com/autroshot/cinema">GitHub 링크</a>
+        </div>
+      </footer>
     </Container>
   );
 }

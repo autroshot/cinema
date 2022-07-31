888ed93e28b6f6926391647fa8f483622e141a12
autroshot <autroshot@gamil.com>
2022-07-31 20:50:53 +0900

    SSRProvider로 앱 감싸기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
+import { SSRProvider } from 'react-bootstrap';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SSRProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SSRProvider>
   );
 }
 

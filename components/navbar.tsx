396719569d01646b346c664a180572a34d8c70b3
autroshot <autroshot@gamil.com>
2022-07-31 15:04:30 +0900

    네비바 배치 1줄로 변경

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,58 @@
 import Image from 'next/image';
 import logo from '../public/logo.png';
-import { Container, Nav, Navbar as BootstrapNavBar } from 'react-bootstrap';
+import {
+  Container,
+  Nav,
+  Navbar as BootstrapNavBar,
+  NavDropdown,
+} from 'react-bootstrap';
 import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
   return (
     <Container>
-      <BootstrapNavBar expand="lg" bg="light" className="p-0">
+      <BootstrapNavBar expand="md" bg="light" className="p-0">
         <Container className="p-0">
           <BootstrapNavBar.Brand className="mx-3">
             <Image src={logo} alt="시네마 로고" width={40} height={40} />
             <span className={styles.logoText}>CINEMA</span>
           </BootstrapNavBar.Brand>
-          <div className="me-auto flex-column">
-            <Nav className="flex-row">
-              <Nav.Link className="me-3">회원가입</Nav.Link>
-              <Nav.Link>로그인</Nav.Link>
+          <BootstrapNavBar.Toggle aria-controls="basic-navbar-nav" />
+          <BootstrapNavBar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown title="예매" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">예매하기</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  상영시간표
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="영화" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">홈</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.2">
+                  현재상영작
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  상영예정작
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="영화관" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">월드타워</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">건대입구</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">수원</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">노원</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="스페셜관" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">홈</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.2">샤롯데</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  수퍼플렉스G
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">컬러리움</NavDropdown.Item>
+              </NavDropdown>
             </Nav>
-            <Nav className="flex-row">
-              <Nav.Link className="me-3">예매</Nav.Link>
-              <Nav.Link className="me-3">영화</Nav.Link>
-              <Nav.Link>영화관</Nav.Link>
-            </Nav>
-          </div>
+          </BootstrapNavBar.Collapse>
         </Container>
       </BootstrapNavBar>
     </Container>

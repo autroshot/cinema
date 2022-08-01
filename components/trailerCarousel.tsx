95959da9a436fda55317b4b2512547b211497b70
autroshot <autroshot@gamil.com>
2022-08-01 14:50:07 +0900

    트레일러 모달 일부 구현

--- components/trailerCarousel.tsx
+++ components/trailerCarousel.tsx
@@ -15,41 +15,64 @@ export default function TrailerCarousel() {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <Swiper
-      modules={[Navigation]}
-      spaceBetween={0}
-      slidesPerView={1}
-      navigation
-      loop
-    >
-      <SwiperSlide className={styles.trailerSlide}>
-        <button
-          type="button"
-          className={styles.trailerButton}
-          onClick={() => console.log('clicked')}
-        >
-          <Image src={trailer1} alt="리미트 트레일러" />
-          <div className="position-absolute top-50 start-50 translate-middle">
-            <Image src={playBtn} alt="플레이 버튼" />
-          </div>
-        </button>
-      </SwiperSlide>
-      <SwiperSlide className={styles.trailerSlide}>
-        <button type="button" className={styles.trailerButton}>
-          <Image src={trailer2} alt="불릿트레인 트레일러" />
-          <div className="position-absolute top-50 start-50 translate-middle">
-            <Image src={playBtn} alt="플레이 버튼" />
-          </div>
-        </button>
-      </SwiperSlide>
-      <SwiperSlide className={styles.trailerSlide}>
-        <button type="button" className={styles.trailerButton}>
-          <Image src={trailer3} alt="한산 트레일러" />
-          <div className="position-absolute top-50 start-50 translate-middle">
-            <Image src={playBtn} alt="플레이 버튼" />
-          </div>
-        </button>
-      </SwiperSlide>
-    </Swiper>
+    <>
+      <Swiper
+        modules={[Navigation]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        loop
+      >
+        <SwiperSlide className={styles.trailerSlide}>
+          <button
+            type="button"
+            className={styles.trailerButton}
+            onClick={handleClick}
+          >
+            <Image src={trailer1} alt="리미트 트레일러" />
+            <div className="position-absolute top-50 start-50 translate-middle">
+              <Image src={playBtn} alt="플레이 버튼" />
+            </div>
+          </button>
+        </SwiperSlide>
+        <SwiperSlide className={styles.trailerSlide}>
+          <button type="button" className={styles.trailerButton}>
+            <Image src={trailer2} alt="불릿트레인 트레일러" />
+            <div className="position-absolute top-50 start-50 translate-middle">
+              <Image src={playBtn} alt="플레이 버튼" />
+            </div>
+          </button>
+        </SwiperSlide>
+        <SwiperSlide className={styles.trailerSlide}>
+          <button type="button" className={styles.trailerButton}>
+            <Image src={trailer3} alt="한산 트레일러" />
+            <div className="position-absolute top-50 start-50 translate-middle">
+              <Image src={playBtn} alt="플레이 버튼" />
+            </div>
+          </button>
+        </SwiperSlide>
+      </Swiper>
+      <Modal
+        size="lg"
+        show={showModal}
+        onHide={handleHide}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-lg">
+            Large Modal
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>...</Modal.Body>
+      </Modal>
+    </>
   );
+
+  function handleClick() {
+    setShowModal(true);
+  }
+
+  function handleHide() {
+    setShowModal(false);
+  }
 }

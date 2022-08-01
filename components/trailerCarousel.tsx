d3f1353f9045da031c1b573de56ba3fa45e7feb3
autroshot <autroshot@gamil.com>
2022-08-01 14:35:02 +0900

    트레일러에 플레이 버튼 추가

--- components/trailerCarousel.tsx
+++ components/trailerCarousel.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import trailer1 from '../public/dummy/carousel-trailer-1.jpg';
 import trailer2 from '../public/dummy/carousel-trailer-2.jpg';
 import trailer3 from '../public/dummy/carousel-trailer-3.jpg';
+import playBtn from '../public/play-btn.png';
 import styles from '../styles/TrailerCarousel.module.css';
 
 export default function TrailerCarousel() {
@@ -21,16 +22,25 @@ export default function TrailerCarousel() {
       <SwiperSlide className={styles.trailerSlide}>
         <button type="button" className={styles.trailerButton}>
           <Image src={trailer1} alt="리미트 트레일러" />
+          <div className="position-absolute top-50 start-50 translate-middle">
+            <Image src={playBtn} alt="플레이 버튼" />
+          </div>
         </button>
       </SwiperSlide>
       <SwiperSlide className={styles.trailerSlide}>
         <button type="button" className={styles.trailerButton}>
           <Image src={trailer2} alt="불릿트레인 트레일러" />
+          <div className="position-absolute top-50 start-50 translate-middle">
+            <Image src={playBtn} alt="플레이 버튼" />
+          </div>
         </button>
       </SwiperSlide>
       <SwiperSlide className={styles.trailerSlide}>
         <button type="button" className={styles.trailerButton}>
           <Image src={trailer3} alt="한산 트레일러" />
+          <div className="position-absolute top-50 start-50 translate-middle">
+            <Image src={playBtn} alt="플레이 버튼" />
+          </div>
         </button>
       </SwiperSlide>
     </Swiper>

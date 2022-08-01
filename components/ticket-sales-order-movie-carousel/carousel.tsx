a1ba8c31cb2c52c861631e79a9c6576ab03c7696
autroshot <autroshot@gamil.com>
2022-08-02 02:59:54 +0900

    영화 예매순 캐러셀 이미지에 라운딩 처리

--- components/ticket-sales-order-movie-carousel/carousel.tsx
+++ components/ticket-sales-order-movie-carousel/carousel.tsx
@@ -5,18 +5,21 @@ import 'swiper/css/navigation';
 import { Card } from 'react-bootstrap';
 import Image from 'next/image';
 import movie1 from '../../public/images/dummy/movies/01.jpg';
+import styles from './carousel.module.css';
 
 export default function Carousel() {
   return (
     <Swiper
       modules={[Navigation]}
-      spaceBetween={10}
+      spaceBetween={15}
       slidesPerView={5}
       navigation
     >
       <SwiperSlide>
-        <Card>
-          <Image src={movie1} alt={'한산'} />
+        <Card className={styles.card}>
+          <div className={styles.imageBox}>
+            <Image src={movie1} alt={'한산'} />
+          </div>
           <Card.Body>
             <Card.Title>한산: 용의 출현</Card.Title>
             <Card.Text>예매율 44.4% | 별 9.4 | 하트</Card.Text>

fc9441a587c0174efc04e2364b1492536e6c0e03
autroshot <autroshot@gamil.com>
2022-08-02 01:31:53 +0900

    미사용 코드 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,19 +15,6 @@ export default function Home() {
       </Head>
       <Container fluid className="p-0">
         <Carousel />
-        {/* <Modal
-          size="lg"
-          show={lgShow}
-          onHide={() => setLgShow(false)}
-          aria-labelledby="example-modal-sizes-title-lg"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="example-modal-sizes-title-lg">
-              Large Modal
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>...</Modal.Body>
-        </Modal> */}
       </Container>
     </div>
   );

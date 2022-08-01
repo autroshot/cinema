db225d9a3f042db8d429a14755d7ce72673e72fc
autroshot <autroshot@gamil.com>
2022-08-01 21:31:10 +0900

    Carousel 컴포넌트의 handleClick 함수 구현

--- components/trailer-carousel/carousel.tsx
+++ components/trailer-carousel/carousel.tsx
@@ -14,7 +14,7 @@ import YoutubeModal from './youtubeModal';
 export default function Carousel() {
   const [showModal, setShowModal] = useState(false);
   const [youtubeId, setYoutubeId] = useState<null | string>(null);
-
+  console.log(youtubeId);
   const SLIDE_PARAMS = [
     { title: '리미트', src: image1, youtubeId: 'H150mI_LPV4' },
     { title: '불릿트레인', src: image2, youtubeId: '_ics0ClH5TQ' },
@@ -31,13 +31,25 @@ export default function Carousel() {
         loop
       >
         <SwiperSlide className={styles.trailerSlide}>
-          <SlideContent title="리미트" src={image1} onClick={handleClick} />
+          <SlideContent
+            title="리미트"
+            src={image1}
+            onClick={() => handleClick('H150mI_LPV4')}
+          />
         </SwiperSlide>
         <SwiperSlide className={styles.trailerSlide}>
-          <SlideContent title="불릿트레인" src={image2} onClick={handleClick} />
+          <SlideContent
+            title="불릿트레인"
+            src={image2}
+            onClick={() => handleClick('_ics0ClH5TQ')}
+          />
         </SwiperSlide>
         <SwiperSlide className={styles.trailerSlide}>
-          <SlideContent title="한산" src={image3} onClick={handleClick} />
+          <SlideContent
+            title="한산"
+            src={image3}
+            onClick={() => handleClick('GduEtmvwcI0')}
+          />
         </SwiperSlide>
       </Swiper>
       <YoutubeModal
@@ -48,8 +60,9 @@ export default function Carousel() {
     </>
   );
 
-  function handleClick() {
+  function handleClick(newYoutubeId: string) {
     setShowModal(true);
+    setYoutubeId(newYoutubeId);
   }
 
   function handleHide() {

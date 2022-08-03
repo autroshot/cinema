68f218f8df000df945e0ad3b00aa3664df93c5ba
autroshot <autroshot@gamil.com>
2022-08-03 18:03:42 +0900

    스페셜관 링크 구현

--- components/specialScreenLinks.tsx
+++ components/specialScreenLinks.tsx
@@ -1,15 +1,34 @@
+import Image from 'next/image';
 import { Col, Row } from 'react-bootstrap';
 import styles from './specialScreenLinks.module.css';
+import superPlexG from '../public/super-plex-g.png';
+import colorium from '../public/colorium.png';
+import super4D from '../public/super-4d.png';
 
 export default function SpecialScreenLinks() {
   return (
-    <Row className={styles.border}>
-      <Col className="d-flex p-0">
-        <h5 className="me-auto">
-          <b>스페셜관</b>
-        </h5>
-        <a href="">더보기 {'>'}</a>
-      </Col>
-    </Row>
+    <>
+      <Row className={styles.border}>
+        <Col className="d-flex p-0">
+          <h5 className="me-auto">
+            <b>스페셜관</b>
+          </h5>
+          <a href="">더보기 {'>'}</a>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex mt-3">
+          <a href="">
+            <Image src={superPlexG} alt="수퍼플렉스G" />
+          </a>
+          <a href="" className="ms-5">
+            <Image src={colorium} alt="컬러리움" />
+          </a>
+          <a href="" className="ms-5">
+            <Image src={super4D} alt="수퍼4D" />
+          </a>
+        </Col>
+      </Row>
+    </>
   );
 }

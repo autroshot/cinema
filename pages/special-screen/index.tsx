5d244d5bcd5c261e252d575164e0d9a283f654da
autroshot <autroshot@gamil.com>
2022-08-05 01:42:51 +0900

    특별관 인덱스 페이지 일부 구현

--- pages/special-screen/index.tsx
+++ pages/special-screen/index.tsx
@@ -1,3 +1,20 @@
+import Image from 'next/image';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+
 export default function Index() {
-  return '특별관 인덱스 페이지';
+  return (
+    <Container className="px-0 py-5">
+      <h1>
+        <b>
+          Special <div className="ms-5">Theater For You</div>
+        </b>
+      </h1>
+      <h5 className="ms-3 mt-3">영화의 순간을 더욱 특별하게</h5>
+      <Row>
+        <Col>
+          <Card></Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }

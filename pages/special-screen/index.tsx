c4d33ea1aab8f45592faf265731173e6f8740493
autroshot <autroshot@gamil.com>
2022-08-05 03:47:32 +0900

    특별관 인덱스 페이지 구현

--- pages/special-screen/index.tsx
+++ pages/special-screen/index.tsx
@@ -14,25 +14,25 @@ export default function Index() {
       </h1>
       <h5 className="ms-3 mt-3">영화의 순간을 더욱 특별하게</h5>
       <div className="mt-5 d-flex justify-content-around flex-wrap">
-        <Card className="mb-4 mb-lg-0">
+        <Card className="mb-4 mb-lg-0 border-0">
           <Image src={superPlexG} alt="수퍼플렉스G" />
           <Card.Body>
             <Card.Title>수퍼플렉스G</Card.Title>
             <Card.Text>기네스북이 인정한 세계 최대 스케일</Card.Text>
           </Card.Body>
         </Card>
-        <Card className="mb-4 mb-lg-0">
-          <Image src={superPlexG} alt="수퍼플렉스G" />
+        <Card className="mb-4 mb-lg-0 border-0">
+          <Image src={colorium} alt="컬러리움" />
           <Card.Body>
-            <Card.Title>수퍼플렉스G</Card.Title>
-            <Card.Text>기네스북이 인정한 세계 최대 스케일</Card.Text>
+            <Card.Title>컬러리움</Card.Title>
+            <Card.Text>세상의 모든 색으로 영화를 보다</Card.Text>
           </Card.Body>
         </Card>
-        <Card>
-          <Image src={superPlexG} alt="수퍼플렉스G" />
+        <Card className="border-0">
+          <Image src={cineCouple} alt="씨네 커플" />
           <Card.Body>
-            <Card.Title>수퍼플렉스G</Card.Title>
-            <Card.Text>기네스북이 인정한 세계 최대 스케일</Card.Text>
+            <Card.Title>씨네 커플</Card.Title>
+            <Card.Text>연인을 위한 두근두근 커플석</Card.Text>
           </Card.Body>
         </Card>
       </div>

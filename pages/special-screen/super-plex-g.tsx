652dd2484d6126226444e071f0f7e3f5753f4b37
autroshot <autroshot@gamil.com>
2022-08-05 14:02:18 +0900

    수퍼플렉스G 페이지의 비디오 CSS 구현

--- pages/special-screen/super-plex-g.tsx
+++ pages/special-screen/super-plex-g.tsx
@@ -1,16 +1,20 @@
 import { Container } from 'react-bootstrap';
+import styles from './super-plex-g.module.css';
 
 export default function SuperPlexG() {
   return (
     <>
-      <Container fluid>
-        <video
-          src="/videos/video-super-plex-g.mp4"
-          muted
-          autoPlay
-          loop
-          playsInline
-        />
+      <Container fluid className="p-0 bg-black">
+        <div className="d-flex justify-content-center">
+          <video
+            className={styles.video}
+            src="/videos/video-super-plex-g.mp4"
+            muted
+            autoPlay
+            loop
+            playsInline
+          />
+        </div>
       </Container>
       <Container></Container>
     </>

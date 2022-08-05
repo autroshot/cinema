4232aeb7b28a88a0c2e7b8feb261f9dbc7e7b5ea
autroshot <autroshot@gamil.com>
2022-08-05 14:30:31 +0900

    수퍼플렉스G 페이지의 로고 CSS 일부 구현

--- pages/special-screen/super-plex-g.tsx
+++ pages/special-screen/super-plex-g.tsx
@@ -4,7 +4,7 @@ import styles from './super-plex-g.module.css';
 export default function SuperPlexG() {
   return (
     <>
-      <Container fluid className="p-0 bg-black">
+      <Container fluid className="position-relative p-0 bg-black">
         <div className="d-flex justify-content-center">
           <video
             className={styles.video}
@@ -14,6 +14,9 @@ export default function SuperPlexG() {
             loop
             playsInline
           />
+        </div>
+        <div className="position-absolute top-50 start-50 translate-middle w-100 h-100">
+          <Container>hello</Container>
         </div>
       </Container>
       <Container></Container>

3fc076fa2344fc449e8e0f0527b5bd60e49ba501
autroshot <autroshot@gamil.com>
2022-08-05 12:01:51 +0900

    인덱스 페이지에 특별관 상세 페이지 링크 구현

--- components/specialScreenLinks.tsx
+++ components/specialScreenLinks.tsx
@@ -14,20 +14,28 @@ export default function SpecialScreenLinks() {
           <h5 className="me-auto">
             <b>스페셜관</b>
           </h5>
-          <Link href="/special-screen/">더보기 &gt;</Link>
+          <Link href="/special-screen/">
+            <a>더보기 &gt;</a>
+          </Link>
         </Col>
       </Row>
       <Row>
         <Col className="mt-3 d-flex d-flex justify-content-between justify-content-lg-start">
-          <a href="">
-            <Image src={superPlexG} alt="수퍼플렉스G" />
-          </a>
-          <a href="" className="ms-lg-5">
-            <Image src={colorium} alt="컬러리움" />
-          </a>
-          <a href="" className="ms-lg-5">
-            <Image src={super4D} alt="수퍼4D" />
-          </a>
+          <Link href="/special-screen/super-plex-g">
+            <a>
+              <Image src={superPlexG} alt="수퍼플렉스G" />
+            </a>
+          </Link>
+          <Link href="/special-screen/colorium">
+            <a className="ms-lg-5">
+              <Image src={colorium} alt="컬러리움" />
+            </a>
+          </Link>
+          <Link href="/special-screen/cine-couple">
+            <a className="ms-lg-5">
+              <Image src={super4D} alt="씨네 커플" />
+            </a>
+          </Link>
         </Col>
       </Row>
     </>

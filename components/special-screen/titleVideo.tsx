2af02eed0c88f00dc6366d29e88ce5dca3ce6f25
autroshot <autroshot@gamil.com>
2022-08-05 20:36:41 +0900

    특별관 수퍼플렉스G 페이지 구현. TitleVideo 컴포넌트 구현

---
+++ components/special-screen/titleVideo.tsx
@@ -0,0 +1,35 @@
+import Image, { StaticImageData } from 'next/image';
+import { Container } from 'react-bootstrap';
+import styles from './titleVideo.module.css';
+
+export default function TitleVideo(props: Props) {
+  return (
+    <>
+      <div className="d-flex justify-content-center">
+        <video
+          className={styles.video}
+          src={props.src}
+          muted
+          autoPlay
+          loop
+          playsInline
+        />
+      </div>
+      <div className="position-absolute top-50 start-50 translate-middle w-100 h-100">
+        <Container className="h-100 d-flex flex-column">
+          <div className="mt-auto mb-3">
+            <Image src={props.logo} alt={props.alt} />
+            <h5 className="text-white mt-3 ms-5">{props.text}</h5>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+interface Props {
+  src: string;
+  logo: StaticImageData;
+  alt: string;
+  text: string;
+}
--- pages/special-screen/super-plex-g.tsx
+++ pages/special-screen/super-plex-g.tsx
@@ -1,8 +1,11 @@
 import { Col, Container, Row } from 'react-bootstrap';
-import styles from './super-plex-g.module.css';
 import Image from 'next/image';
 import logo from '../../public/logos/special-screen/super-plex-g-white.png';
+import image1 from '../../public/images/special-screen/super-plex-g-detail-1.jpg';
+import image2 from '../../public/images/special-screen/super-plex-g-detail-2.jpg';
+import image3 from '../../public/images/special-screen/super-plex-g-detail-3.jpg';
 import Link from 'next/link';
+import styles from './super-plex-g.module.css';
 
 export default function SuperPlexG() {
   return (
@@ -80,6 +83,67 @@ export default function SuperPlexG() {
               </a>
             </Link>
           </Row>
+        </section>
+        <section>
+          <article className={styles.articleLeft}>
+            <h3 className={styles.textGray}>
+              <b>
+                The Largest <div className={styles.textLarge}>Screen</div>
+              </b>
+            </h3>
+            <h3 className="mt-3">
+              &apos;14 기네스 월드 레코드
+              <br />
+              세계 최대 스크린
+            </h3>
+            <p className="my-3">
+              34M X 13.8M 초대형 스크린은 세계 어디에서도 느낄 수 없는 압도적인
+              스케일로
+              <br />
+              몰입의 정점을 완성합니다.
+            </p>
+            <Image src={image1} alt="세계 최대 스크린" />
+          </article>
+          <article className={styles.articleRight}>
+            <h3 className={styles.textGray}>
+              <b>
+                <div className={styles.textLarge}>Laser</div>
+              </b>
+            </h3>
+            <h3 className="mt-3">듀얼 6P 레이저 영사기</h3>
+            <p className="my-3">
+              국내 최초로 도입된 듀얼 6P 레이저 영사기는
+              <br />
+              부드럽고 선명한 화질과 기존 영사기의 1.6배 이상의 밝기를
+              제공합니다.
+            </p>
+            <Image src={image2} alt="듀얼 6P 레이저 영사기" />
+          </article>
+          <article className={styles.articleLeft}>
+            <div className="float-start">
+              <h3 className={styles.textGray}>
+                <b>
+                  <div className={styles.textLarge}>Dolby</div>
+                </b>
+              </h3>
+              <h3 className="mt-3">
+                돌비 애트모스 &amp;
+                <br />
+                165 어레이 스피커
+              </h3>
+              <p className="my-3">
+                전 좌석 360도 입체 음향을 전하는 돌비 애트모스
+                <br />
+                165개에 달하는 국내 최대 스피커 시스템은
+                <br />
+                웅장하고 리얼한 입체 사운드를 구현합니다.
+              </p>
+            </div>
+            <div className="float-end">
+              <Image src={image3} alt="돌비 애트모스 스피커" />
+            </div>
+            <div className="clearfix" />
+          </article>
         </section>
       </Container>
     </>

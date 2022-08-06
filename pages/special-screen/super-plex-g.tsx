30a999100a6accb0bb3685009e96cc55fa451846
autroshot <autroshot@gamil.com>
2022-08-06 15:29:30 +0900

    리팩터링: 수퍼플렉스G 페이지에서 Layout 컴포넌트 추출

--- pages/special-screen/super-plex-g.tsx
+++ pages/special-screen/super-plex-g.tsx
@@ -7,6 +7,7 @@ import image1 from '../../public/images/special-screen/super-plex-g-detail-1.jpg
 import image2 from '../../public/images/special-screen/super-plex-g-detail-2.jpg';
 import image3 from '../../public/images/special-screen/super-plex-g-detail-3.jpg';
 import styles from './common.module.css';
+import Layout from '../../components/special-screen/layout';
 
 export default function SuperPlexG() {
   const THEATER_LINKS_DUMMY = [
@@ -21,89 +22,72 @@ export default function SuperPlexG() {
   ];
 
   return (
-    <>
-      <Container fluid className="position-relative p-0 bg-black">
-        <TitleWithVideo
-          src="https://drive.google.com/uc?export=download&id=1aPuTEWEA6-wnLMGhpeTaKQvitR95lXQo"
-          poster="/images/special-screen/super-plex-g-video-poster.jpg"
-          logo={logo}
-          alt="수퍼플렉스G"
-          text="기네스북이 인정한 세계 최대 스케일"
-        />
-      </Container>
-      <Container className="my-5">
-        <Row className="mb-2">
-          <Col>
-            <h5>
-              <b>수퍼플렉스G 상영시간표</b>
-            </h5>
-          </Col>
-        </Row>
-        <section>
-          <TheaterLinks theaterLinks={THEATER_LINKS_DUMMY} />
-        </section>
-        <section>
-          <article className={styles.articleLeft}>
-            <h3 className={styles.textGray}>
-              <b>
-                The Largest <div className={styles.textLarge}>Screen</div>
-              </b>
-            </h3>
-            <h3 className="mt-3">
-              &apos;14 기네스 월드 레코드
-              <br />
-              세계 최대 스크린
-            </h3>
-            <p className="my-3">
-              34M X 13.8M 초대형 스크린은 세계 어디에서도 느낄 수 없는 압도적인
-              스케일로
-              <br />
-              몰입의 정점을 완성합니다.
-            </p>
-            <Image src={image1} alt="세계 최대 스크린" />
-          </article>
-          <article className={styles.articleRight}>
-            <h3 className={styles.textGray}>
-              <b>
-                <div className={styles.textLarge}>Laser</div>
-              </b>
-            </h3>
-            <h3 className="mt-3">듀얼 6P 레이저 영사기</h3>
-            <p className="my-3">
-              국내 최초로 도입된 듀얼 6P 레이저 영사기는
-              <br />
-              부드럽고 선명한 화질과 기존 영사기의 1.6배 이상의 밝기를
-              제공합니다.
-            </p>
-            <Image src={image2} alt="듀얼 6P 레이저 영사기" />
-          </article>
-          <article className={styles.articleLeft}>
-            <div className="float-start">
-              <h3 className={styles.textGray}>
-                <b>
-                  <div className={styles.textLarge}>Dolby</div>
-                </b>
-              </h3>
-              <h3 className="mt-3">
-                돌비 애트모스 &amp;
-                <br />
-                165 어레이 스피커
-              </h3>
-              <p className="my-3">
-                전 좌석 360도 입체 음향을 전하는 돌비 애트모스
-                <br />
-                165개에 달하는 국내 최대 스피커 시스템은
-                <br />
-                웅장하고 리얼한 입체 사운드를 구현합니다.
-              </p>
-            </div>
-            <div className="float-end">
-              <Image src={image3} alt="돌비 애트모스 스피커" />
-            </div>
-            <div className="clearfix" />
-          </article>
-        </section>
-      </Container>
-    </>
+    <Layout
+      src="https://drive.google.com/uc?export=download&id=1aPuTEWEA6-wnLMGhpeTaKQvitR95lXQo"
+      poster="/images/special-screen/super-plex-g-video-poster.jpg"
+      logo={logo}
+      alt="수퍼플렉스G"
+      text="기네스북이 인정한 세계 최대 스케일"
+      theaterLinks={THEATER_LINKS_DUMMY}
+    >
+      <article className={styles.articleLeft}>
+        <h3 className={styles.textGray}>
+          <b>
+            The Largest <div className={styles.textLarge}>Screen</div>
+          </b>
+        </h3>
+        <h3 className="mt-3">
+          &apos;14 기네스 월드 레코드
+          <br />
+          세계 최대 스크린
+        </h3>
+        <p className="my-3">
+          34M X 13.8M 초대형 스크린은 세계 어디에서도 느낄 수 없는 압도적인
+          스케일로
+          <br />
+          몰입의 정점을 완성합니다.
+        </p>
+        <Image src={image1} alt="세계 최대 스크린" />
+      </article>
+      <article className={styles.articleRight}>
+        <h3 className={styles.textGray}>
+          <b>
+            <div className={styles.textLarge}>Laser</div>
+          </b>
+        </h3>
+        <h3 className="mt-3">듀얼 6P 레이저 영사기</h3>
+        <p className="my-3">
+          국내 최초로 도입된 듀얼 6P 레이저 영사기는
+          <br />
+          부드럽고 선명한 화질과 기존 영사기의 1.6배 이상의 밝기를 제공합니다.
+        </p>
+        <Image src={image2} alt="듀얼 6P 레이저 영사기" />
+      </article>
+      <article className={styles.articleLeft}>
+        <div className="float-start">
+          <h3 className={styles.textGray}>
+            <b>
+              <div className={styles.textLarge}>Dolby</div>
+            </b>
+          </h3>
+          <h3 className="mt-3">
+            돌비 애트모스 &amp;
+            <br />
+            165 어레이 스피커
+          </h3>
+          <p className="my-3">
+            전 좌석 360도 입체 음향을 전하는 돌비 애트모스
+            <br />
+            165개에 달하는 국내 최대 스피커 시스템은
+            <br />
+            웅장하고 리얼한 입체 사운드를 구현합니다.
+          </p>
+        </div>
+        <div className="float-end">
+          <Image src={image3} alt="돌비 애트모스 스피커" />
+        </div>
+        <div className="clearfix" />
+      </article>
+    </Layout>
   );
 }

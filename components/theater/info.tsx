8592d641bbe760f5787c9316a438a768135e0a69
autroshot <autroshot@gamil.com>
2022-08-08 18:28:09 +0900

    영화관의 Info 컴포넌트 일부 구현

--- components/theater/info.tsx
+++ components/theater/info.tsx
@@ -1,3 +1,41 @@
+import { Col, Row } from 'react-bootstrap';
+
 export default function Info() {
-  return <div>영화관 정보</div>;
+  return (
+    <>
+      <Row>
+        <Col>
+          <b>&middot;</b> 총 상영관 수 <b>21개관</b>
+        </Col>
+        <Col>
+          <b>&middot;</b> 총 좌석수 <b>4,609석</b>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col>
+          <b>서울 송파구 올림픽로 300 (신천동) 5층-11층</b>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col>
+          <span role="button">
+            <span className="material-symbols-outlined">directions_subway</span>{' '}
+            대중교통 안내
+          </span>
+        </Col>
+        <Col>
+          <span role="button">
+            <span className="material-symbols-outlined">directions_car</span>{' '}
+            자가용/주차안내
+          </span>
+        </Col>
+        <Col>
+          <span role="button">
+            <span className="material-symbols-outlined">location_on</span>{' '}
+            지도보기
+          </span>
+        </Col>
+      </Row>
+    </>
+  );
 }

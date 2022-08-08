b14b0683eb466890d9f4c9b06e9b94e8e798ccd6
autroshot <autroshot@gamil.com>
2022-08-08 23:47:38 +0900

    영화관 모달 교통 안내 일부 구현

--- components/theater/TheaterModal.tsx
+++ components/theater/TheaterModal.tsx
@@ -1,5 +1,7 @@
+import { Col, Row } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
 import { modalType } from './info';
+import styles from './TheaterModal.module.css';
 
 export default function TheaterModal(props: Props) {
   let content = '';
@@ -7,7 +9,7 @@ export default function TheaterModal(props: Props) {
     case null:
       break;
     case '대중교통 안내':
-      content = '지하철로 오시는 길...';
+      content = `- [8호선 잠실역]\n- 8호선 개찰구 통과 후 지하 1층 대합실 이동(10번, 11번 출구 방면) > 우측 10M 앞 쇼핑몰 게이트 통과(10번 11번 출구 사이 통로) > 아쿠아리움 출구 옆 엘리베이터 이용\n- [2호선 잠실역]\n- <롯데몰 영업시간 내> 2호선 출구 중앙 잠실광역버스 환승센터 방면(독도 조형물 입구 앞) > 좌측 쇼핑몰 게이트(삼송빵집 옆)통과 후 우측 방면 > 라인캐릭터 샵 / 전망대 입구 사이 300M 직진 > 아쿠아리움 출구 옆 엘리베이터 이용\n- <롯데몰 영업시간 외> 1층 지상으로 이동(1번 출구) > 약 300m 직진 > 송파구청(8호선 11,12번 출구) 방면 엔제리너스 옆 쇼핑몰 게이트 통과 > 에잇세컨즈/유니클로 사이 엘리베이터 이용`;
       break;
     case '자가용/주차 안내':
       content = '자가용으로 오시는 길...';
@@ -28,7 +30,19 @@ export default function TheaterModal(props: Props) {
       <Modal.Header closeButton>
         <Modal.Title>{props.type}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>{content}</Modal.Body>
+      <Modal.Body>
+        <Row>
+          <Col className="col-auto">
+            <span className="material-symbols-outlined">directions_subway</span>
+          </Col>
+          <Col>
+            <span>
+              <h5>지하철로 오시는 길</h5>
+              <p className={styles.whiteSpace}>{content}</p>
+            </span>
+          </Col>
+        </Row>
+      </Modal.Body>
     </Modal>
   );
 }

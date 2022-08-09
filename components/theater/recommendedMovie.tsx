1052d3c7664d96106a42a48deb9604be28043b9d
autroshot <autroshot@gamil.com>
2022-08-09 17:10:27 +0900

    영화관 페이지의 추천 영화 일부 구현

--- components/theater/recommendedMovie.tsx
+++ components/theater/recommendedMovie.tsx
@@ -1,3 +1,51 @@
+import Image from 'next/image';
+import { Button, Col, Row } from 'react-bootstrap';
+import movieImage from '../../public/images/dummy/movies/01.jpg';
+import styles from './recommendedMovie.module.css';
+
 export default function RecommendedMovie() {
-  return <div>추천 영화</div>;
+  return (
+    <section className="bg-light p-3 rounded-2">
+      <Row>
+        <Col className="col">
+          <div>
+            autroshot님
+            <br />
+            <b>월드타워</b>에서
+            <br />
+            <span className={styles.movieTitle}>한산: 용의 출현</span>
+            <br />
+            어떠세요?
+          </div>
+          <hr />
+          <div>
+            한산: 용의 출현
+            <br />
+            예매율 33.5%{' '}
+            <span className="material-symbols-outlined">
+              <span className={styles.grade}>grade</span>
+            </span>{' '}
+            9.4
+          </div>
+          <hr />
+          <div>
+            <span className="material-symbols-outlined me-3">share</span>
+            <span className="material-symbols-outlined me-3">
+              <span className={styles.favorite}>favorite</span>
+            </span>
+            <Button className="btn-sm btn-primary">예매하기</Button>
+          </div>
+        </Col>
+        <Col className="text-center">
+          <Image
+            src={movieImage}
+            alt="한산: 용의 출현"
+            width={178.5}
+            height={255}
+            className="float-end"
+          />
+        </Col>
+      </Row>
+    </section>
+  );
 }

171cf3a6ec758918d2a50897715f73e733c2df80
autroshot <autroshot@gamil.com>
2022-08-09 20:06:07 +0900

    shareURL 컴포넌트의 onClick에 핸들러 등록

--- components/theater/shareURL.tsx
+++ components/theater/shareURL.tsx
@@ -7,14 +7,18 @@ export default function ShareURL() {
   return (
     <OverlayTrigger
       placement="top"
-      show={false}
+      show={show}
       overlay={
         <Popover id="popover-url">
           <Popover.Body>URL이 복사되었습니다.</Popover.Body>
         </Popover>
       }
     >
-      <span className="material-symbols-outlined me-3" role="button">
+      <span
+        className="material-symbols-outlined me-3"
+        role="button"
+        onClick={() => setShow(true)}
+      >
         share
       </span>
     </OverlayTrigger>

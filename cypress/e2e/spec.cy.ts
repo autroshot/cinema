976c78e4acfcb5a42e0277bbc97c2c469f0bed67
autroshot <autroshot@gamil.com>
2022-08-17 10:53:51 +0900

    테스트: 페이지 방문 작성

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,16 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+describe('페이지 방문', () => {
+  it('홈페이지', () => {
+    cy.visit('/');
+  });
+  it('스페셜관 인덱스', () => {
+    cy.visit('/');
+    cy.contains('스페셜관').click();
+    cy.contains('홈').click();
+    cy.url().should('include', '/special-screen');
+
+    cy.visit('/');
+    cy.contains('더보기 >').click();
+    cy.url().should('include', '/special-screen');
   });
 });
 

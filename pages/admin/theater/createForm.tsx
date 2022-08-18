ef3794beeeab5627ffdb74f975d0dfb6a79da7bd
autroshot <autroshot@gamil.com>
2022-08-19 00:42:26 +0900

    createForm 컴포넌트에 form 태그 추가, 버튼에 type 속성 추가

--- pages/admin/theater/createForm.tsx
+++ pages/admin/theater/createForm.tsx
@@ -16,133 +16,135 @@ export default function CreateForm() {
   return (
     <Container className="mt-4">
       <h3>새 영화관 등록</h3>
-      <Table className={styles.table}>
-        <colgroup>
-          <col className={styles.fieldCol} />
-          <col className={styles.valueCol} />
-        </colgroup>
-        <thead>
-          <tr>
-            <th>필드</th>
-            <th>값</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <label htmlFor="name">name</label>
-            </td>
-            <td>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                className="w-100"
-                value={values.name}
-                onInput={handleChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="street_address">street_address</label>
-            </td>
-            <td>
-              <input
-                id="street_address"
-                name="street_address"
-                type="text"
-                className="w-100"
-                value={values.street_address}
-                onInput={handleChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="kakao_map_id">kakao_map_id</label>
-            </td>
-            <td>
-              <input
-                id="kakao_map_id"
-                name="kakao_map_id"
-                type="text"
-                className="w-100"
-                value={values.kakao_map_id}
-                onInput={handleChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="subway">subway</label>
-            </td>
-            <td>
-              <textarea
-                id="subway"
-                name="subway"
-                rows={10}
-                className="w-100"
-                value={values.subway}
-                onInput={handleChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="bus">bus</label>
-            </td>
-            <td>
-              <textarea
-                id="bus"
-                name="bus"
-                className="w-100"
-                rows={10}
-                value={values.bus}
-                onInput={handleChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="car">car</label>
-            </td>
-            <td>
-              <textarea
-                id="car"
-                name="car"
-                className="w-100"
-                rows={10}
-                value={values.car}
-                onInput={handleChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label htmlFor="parking">parking</label>
-            </td>
-            <td>
-              <textarea
-                id="parking"
-                name="parking"
-                className="w-100"
-                rows={10}
-                value={values.parking}
-                onInput={handleChange}
-              />
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <Row className="mb-3">
-        <Col>
-          <Button>등록</Button>
-          <Button variant="secondary" className="ms-3">
-            취소
-          </Button>
-        </Col>
-      </Row>
+      <form>
+        <Table className={styles.table}>
+          <colgroup>
+            <col className={styles.fieldCol} />
+            <col className={styles.valueCol} />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>필드</th>
+              <th>값</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <label htmlFor="name">name</label>
+              </td>
+              <td>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  className="w-100"
+                  value={values.name}
+                  onInput={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="street_address">street_address</label>
+              </td>
+              <td>
+                <input
+                  id="street_address"
+                  name="street_address"
+                  type="text"
+                  className="w-100"
+                  value={values.street_address}
+                  onInput={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="kakao_map_id">kakao_map_id</label>
+              </td>
+              <td>
+                <input
+                  id="kakao_map_id"
+                  name="kakao_map_id"
+                  type="text"
+                  className="w-100"
+                  value={values.kakao_map_id}
+                  onInput={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="subway">subway</label>
+              </td>
+              <td>
+                <textarea
+                  id="subway"
+                  name="subway"
+                  rows={10}
+                  className="w-100"
+                  value={values.subway}
+                  onInput={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="bus">bus</label>
+              </td>
+              <td>
+                <textarea
+                  id="bus"
+                  name="bus"
+                  className="w-100"
+                  rows={10}
+                  value={values.bus}
+                  onInput={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="car">car</label>
+              </td>
+              <td>
+                <textarea
+                  id="car"
+                  name="car"
+                  className="w-100"
+                  rows={10}
+                  value={values.car}
+                  onInput={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label htmlFor="parking">parking</label>
+              </td>
+              <td>
+                <textarea
+                  id="parking"
+                  name="parking"
+                  className="w-100"
+                  rows={10}
+                  value={values.parking}
+                  onInput={handleChange}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+        <Row className="mb-3">
+          <Col>
+            <Button type="submit">등록</Button>
+            <Button type="button" variant="secondary" className="ms-3">
+              취소
+            </Button>
+          </Col>
+        </Row>
+      </form>
     </Container>
   );
 

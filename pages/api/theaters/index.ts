4e8526d2f70b38fc6263efe9b98b0afc09198db0
autroshot <autroshot@gamil.com>
2022-08-20 19:54:49 +0900

    리팩토링: 영화관 api if문 switch문으로 변경

--- pages/api/theaters/index.ts
+++ pages/api/theaters/index.ts
@@ -6,29 +6,36 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  if (req.method === 'POST') {
-    try {
-      const body = req.body as RequestData;
+  switch (req.method) {
+    case 'GET':
+      break;
 
-      await prisma.theater.create({
-        data: { ...body },
-      });
+    case 'POST':
+      try {
+        const body = req.body as RequestData;
 
-      res.status(201).end();
-    } catch (err) {
-      console.error(err);
+        await prisma.theater.create({
+          data: { ...body },
+        });
 
-      if (
-        err instanceof Prisma.PrismaClientKnownRequestError &&
-        err.code === 'P2002'
-      ) {
-        res.status(500).json({ message: '고유 제약 조건 오류' });
-      } else {
-        res.status(500).json({ message: '서버 오류' });
+        res.status(201).end();
+      } catch (err) {
+        console.error(err);
+
+        if (
+          err instanceof Prisma.PrismaClientKnownRequestError &&
+          err.code === 'P2002'
+        ) {
+          res.status(500).json({ message: '고유 제약 조건 오류' });
+        } else {
+          res.status(500).json({ message: '서버 오류' });
+        }
       }
-    }
-  } else {
-    res.status(405).end();
+      break;
+
+    default:
+      res.status(405).end();
+      break;
   }
 }
 

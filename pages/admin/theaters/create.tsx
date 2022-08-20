db030ddc0befb76b410d996416ec47e110493ef0
autroshot <autroshot@gamil.com>
2022-08-20 13:16:19 +0900

    리팩토링: 영화관 등록에서 NoticeModal 컴포넌트 추출

--- pages/admin/theaters/create.tsx
+++ pages/admin/theaters/create.tsx
@@ -1,4 +1,5 @@
 import { Prisma } from '@prisma/client';
+import NoticeModal from 'components/admin/noticeModal';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import {
@@ -190,23 +191,7 @@ export default function CreateForm() {
           </Row>
         </form>
       </Container>
-      <Modal
-        size="sm"
-        show={showCompletion}
-        onHide={handleClose}
-        animation={false}
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>알림</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>등록이 완료되었습니다.</Modal.Body>
-        <Modal.Footer className="justify-content-center p-2">
-          <Button variant="primary" onClick={handleClose}>
-            확인
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <NoticeModal show={showCompletion} onClose={handleClose} />
     </>
   );
 

a1d2d79dcabeda0fcb508768a0f58f34f9c9f7c4
autroshot <autroshot@gamil.com>
2022-08-21 11:31:57 +0900

    리팩토링: 관리자 영화관 목록에서 theaters 컴포넌트 추출

---
+++ components/admin/theaters.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import { GetRequestData, GetResponseData } from 'pages/api/theaters';
+import { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import styles from './theaters.module.css';
+
+export default function Theaters() {
+  const [theaters, setTheaters] = useState<null | GetResponseData>(null);
+
+  useEffect(() => {
+    const orderBy: GetRequestData = { id: 'asc' };
+    const queryString = new URLSearchParams(orderBy).toString();
+
+    fetch(`/api/theaters?${queryString}`)
+      .then((res) => res.json())
+      .then((theaters: GetResponseData) => {
+        setTheaters(theaters);
+      });
+  }, []);
+
+  if (theaters === null) {
+    return (
+      <tr>
+        <td colSpan={2} className="text-center">
+          <Spinner animation="border" size="sm" role="status">
+            <span className="visually-hidden">불러오는 중...</span>
+          </Spinner>
+        </td>
+      </tr>
+    );
+  } else if (theaters.length === 0) {
+    return (
+      <tr>
+        <td colSpan={2} className="text-center">
+          데이터가 없습니다.
+        </td>
+      </tr>
+    );
+  } else {
+    return (
+      <>
+        {theaters.map((theater) => {
+          return (
+            <Link key={theater.id} href={`/admin/theaters/${theater.id}`}>
+              <tr role="link" className={styles.cursorPointer}>
+                <td>{theater.id}</td>
+                <td>{theater.name}</td>
+              </tr>
+            </Link>
+          );
+        })}
+      </>
+    );
+  }
+}
--- pages/admin/theaters/index.tsx
+++ pages/admin/theaters/index.tsx
@@ -1,55 +1,8 @@
+import Theaters from 'components/admin/theaters';
 import Link from 'next/link';
-import { GetRequestData, GetResponseData } from 'pages/api/theaters';
-import { useEffect, useState } from 'react';
-import { Button, Col, Container, Row, Spinner, Table } from 'react-bootstrap';
-import styles from './index.module.css';
+import { Button, Col, Container, Row, Table } from 'react-bootstrap';
 
 export default function Index() {
-  const [theaters, setTheaters] = useState<null | GetResponseData>(null);
-
-  useEffect(() => {
-    const orderBy: GetRequestData = { id: 'asc' };
-    const queryString = new URLSearchParams(orderBy).toString();
-
-    fetch(`/api/theaters?${queryString}`)
-      .then((res) => res.json())
-      .then((theaters: GetResponseData) => {
-        setTheaters(theaters);
-      });
-  }, []);
-
-  let tableContent: JSX.Element | JSX.Element[];
-  if (theaters === null) {
-    tableContent = (
-      <tr>
-        <td colSpan={2} className="text-center">
-          <Spinner animation="border" size="sm" role="status">
-            <span className="visually-hidden">불러오는 중...</span>
-          </Spinner>
-        </td>
-      </tr>
-    );
-  } else if (theaters.length === 0) {
-    tableContent = (
-      <tr>
-        <td colSpan={2} className="text-center">
-          데이터가 없습니다.
-        </td>
-      </tr>
-    );
-  } else {
-    tableContent = theaters.map((theater) => {
-      return (
-        <Link key={theater.id} href={`/admin/theaters/${theater.id}`}>
-          <tr role="link" className={styles.cursorPointer}>
-            <td>{theater.id}</td>
-            <td>{theater.name}</td>
-          </tr>
-        </Link>
-      );
-    });
-  }
-
   return (
     <Container className="mt-4">
       <Table striped bordered hover>
@@ -60,37 +13,7 @@ export default function Index() {
           </tr>
         </thead>
         <tbody>
-          {tableContent}
-          <Link href="/admin/theaters/100">
-            <tr role="link" className={styles.cursorPointer}>
-              <td>100</td>
-              <td>월드타워</td>
-            </tr>
-          </Link>
-          <Link href="/admin/theaters/101">
-            <tr role="link" className={styles.cursorPointer}>
-              <td>101</td>
-              <td>수원</td>
-            </tr>
-          </Link>
-          <Link href="/admin/theaters/102">
-            <tr role="link" className={styles.cursorPointer}>
-              <td>102</td>
-              <td>건대입구</td>
-            </tr>
-          </Link>
-          <Link href="/admin/theaters/103">
-            <tr role="link" className={styles.cursorPointer}>
-              <td>103</td>
-              <td>노원</td>
-            </tr>
-          </Link>
-          <Link href="/admin/theaters/104">
-            <tr role="link" className={styles.cursorPointer}>
-              <td>104</td>
-              <td>김포공항</td>
-            </tr>
-          </Link>
+          <Theaters />
         </tbody>
       </Table>
       <Row>

0e7e70b098d6e39716a2cd496448040d3bb043e9
autroshot <autroshot@gamil.com>
2022-08-22 21:21:59 +0900

    구현: 영화관 api의 try...catch문 함수 전체에 적용

--- pages/api/theaters/index.ts
+++ pages/api/theaters/index.ts
@@ -6,46 +6,51 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<PostResponseData | GetResponseData>
 ) {
-  switch (req.method) {
-    case 'GET':
-      const orderBy = req.query as GetRequestData;
+  try {
+    switch (req.method) {
+      case 'GET':
+        const orderBy = req.query as GetRequestData;
 
-      const theaters = await prisma.theater.findMany({
-        select: {
-          id: true,
-          name: true,
-        },
-        orderBy: orderBy,
-      });
-
-      res.status(200).json(theaters);
-      break;
-
-    case 'POST':
-      try {
-        const body = req.body as PostRequestData;
-
-        await prisma.theater.create({
-          data: { ...body },
+        const theaters = await prisma.theater.findMany({
+          select: {
+            id: true,
+            name: true,
+          },
+          orderBy: orderBy,
         });
 
-        res.status(201).end();
-      } catch (err) {
-        console.error(err);
-        if (
-          err instanceof Prisma.PrismaClientKnownRequestError &&
-          err.code === 'P2002'
-        ) {
-          res.status(500).json({ message: '고유 제약 조건 오류' });
-        } else {
-          res.status(500).json({ message: '서버 오류' });
-        }
-      }
-      break;
+        res.status(200).json(theaters);
+        break;
 
-    default:
-      res.status(405).end();
-      break;
+      case 'POST':
+        try {
+          const body = req.body as PostRequestData;
+
+          await prisma.theater.create({
+            data: { ...body },
+          });
+
+          res.status(201).end();
+        } catch (err) {
+          console.error(err);
+          if (
+            err instanceof Prisma.PrismaClientKnownRequestError &&
+            err.code === 'P2002'
+          ) {
+            res.status(500).json({ message: '고유 제약 조건 오류' });
+          } else {
+            res.status(500).json({ message: '서버 오류' });
+          }
+        }
+        break;
+
+      default:
+        res.status(405).end();
+        break;
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: '서버 오류' });
   }
 }
 

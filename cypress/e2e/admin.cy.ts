0c8c854984cafbab1e800dd26b4bdf97f9a9efbb
autroshot <autroshot@gamil.com>
2022-08-25 11:41:52 +0900

    테스트: 영화관 U

--- cypress/e2e/admin.cy.ts
+++ cypress/e2e/admin.cy.ts
@@ -1,6 +1,7 @@
 describe('관리자 페이지 방문', () => {
   it('영화관', () => {
     cy.visit('/');
+
     cy.contains('관리자').click();
     cy.contains('관리자 인덱스 페이지');
     cy.contains('영화관').click();
@@ -21,6 +22,9 @@ const THEATER_DUMMY = {
   name: '테스트 영화관',
   street_address: '서울특별시 중구 세종대로 110',
   google_maps_place_id: 'ChIJ81IZg_KifDURyA2TiacuQ3w',
+  bus: '테스트 버스',
+  car: '테스트 자가용',
+  parking: '테스트 주차',
 };
 describe('영화관 CRUD', () => {
   const duplicatedName = '월드타워';
@@ -31,6 +35,7 @@ describe('영화관 CRUD', () => {
 
   it('C', () => {
     cy.visit('http://localhost:3000/admin/theaters/create');
+
     cy.contains('name').click().type(duplicatedName);
     cy.contains('google_maps_place_id')
       .click()
@@ -58,6 +63,7 @@ describe('영화관 CRUD', () => {
 
   it('R', () => {
     cy.visit('http://localhost:3000/admin/theaters');
+
     cy.get('[data-cy="theaters"]').contains('월드타워');
     cy.get('[data-cy="theaters"]').contains('수원');
     cy.get('[data-cy="theaters"]').contains('건대입구');
@@ -65,6 +71,54 @@ describe('영화관 CRUD', () => {
     cy.get('[data-cy="theaters"]').contains('월드타워').click();
     cy.get('[data-cy="title"]').should('have.text', '영화관 상세');
     cy.contains('월드타워');
+  });
+
+  it.only('U', () => {
+    cy.visit('http://localhost:3000/admin/theaters/1');
+
+    cy.contains('google_maps_place_id').click().type('{selectAll}{del}');
+    cy.get('button').contains('수정').click();
+    cy.get('[data-cy="alert"]').should(
+      'include.text',
+      '필숫값이 비어 있습니다.'
+    );
+
+    cy.contains('name')
+      .click()
+      .type('{selectAll}{del}')
+      .type(THEATER_DUMMY.name);
+    cy.contains('street_address')
+      .click()
+      .type('{selectAll}{del}')
+      .type(THEATER_DUMMY.street_address);
+    cy.contains('google_maps_place_id')
+      .click()
+      .type('{selectAll}{del}')
+      .type(THEATER_DUMMY.google_maps_place_id);
+    cy.contains('subway').click().type('{selectAll}{del}');
+    cy.contains('bus').click().type('{selectAll}{del}').type(THEATER_DUMMY.bus);
+    cy.contains('car').click().type('{selectAll}{del}').type(THEATER_DUMMY.car);
+    cy.contains('parking')
+      .click()
+      .type('{selectAll}{del}')
+      .type(THEATER_DUMMY.parking);
+    cy.get('button').contains('수정').click();
+
+    cy.contains('수정이 완료되었습니다.');
+    cy.reload();
+    cy.get('#name').should('have.value', THEATER_DUMMY.name);
+    cy.get('#street_address').should(
+      'have.value',
+      THEATER_DUMMY.street_address
+    );
+    cy.get('#google_maps_place_id').should(
+      'have.value',
+      THEATER_DUMMY.google_maps_place_id
+    );
+    cy.get('#subway').should('have.value', '');
+    cy.get('#bus').should('have.value', THEATER_DUMMY.bus);
+    cy.get('#car').should('have.value', THEATER_DUMMY.car);
+    cy.get('#parking').should('have.value', THEATER_DUMMY.parking);
   });
 });
 

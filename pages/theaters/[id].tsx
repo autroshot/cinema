4d282f1185f432e2a588f0f5a2ca259ed4701ef6
autroshot <autroshot@gamil.com>
2022-08-25 20:11:39 +0900

    구현: 영화관 상세 페이지 정적 생성

--- pages/theaters/[id].tsx
+++ pages/theaters/[id].tsx
@@ -1,17 +1,18 @@
-import { useRouter } from 'next/router';
 import { Col, Container, Row } from 'react-bootstrap';
 import Info from 'components/theater/info';
 import RecommendedMovie from 'components/theater/recommendedMovie';
 import styles from './theater.module.css';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { GetResponseData } from 'pages/api/theaters/[id]';
+import { prisma } from 'db';
 
-export default function Theater() {
-  const router = useRouter();
-  const { id } = router.query;
+export default function Theater({ theater }: Props) {
+  if (theater === null) return <>데이터가 없습니다.</>;
 
   return (
     <Container className="my-3">
-      <div>현재 id: {id}</div>
-      <h1 className="d-inline">월드타워</h1>{' '}
+      <div>현재 id: {theater.id}</div>
+      <h1 className="d-inline">{theater.name}</h1>{' '}
       <span className="material-symbols-outlined">
         <span className={styles.favoriteBorder} role="button">
           favorite
@@ -30,4 +31,37 @@ export default function Theater() {
       <section>상영시간표</section>
     </Container>
   );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const theaters = await prisma.theater.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  const paths = theaters.map((theater) => {
+    return { params: { id: theater.id.toString() } };
+  });
+
+  return { paths: paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = context.params?.id as string;
+  const theater = await prisma.theater.findUnique({
+    where: {
+      id: +id,
+    },
+  });
+
+  return {
+    props: {
+      theater: theater,
+    },
+  };
+};
+
+interface Props {
+  theater: GetResponseData;
 }

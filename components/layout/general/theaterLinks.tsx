bb22d8309072ae9d10fd7aba48f3688f3600ecdc
autroshot <autroshot@gamil.com>
2022-08-25 17:10:49 +0900

    구현: 일반 navbar의 TheaterLinks 컴포넌트

---
+++ components/layout/general/theaterLinks.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { GetRequestData, GetResponseData } from 'pages/api/theaters';
+import { NavDropdown, Spinner } from 'react-bootstrap';
+import useSWR from 'swr';
+
+export default function TheaterLinks() {
+  const orderBy: GetRequestData = { name: 'asc' };
+  const queryString = new URLSearchParams(orderBy).toString();
+
+  const { data, error } = useSWR<GetResponseData>(
+    `/api/theaters?${queryString}`,
+    (url) => fetch(url).then((res) => res.json())
+  );
+
+  if (error) return <>서버 오류</>;
+  if (!data)
+    return (
+      <Spinner animation="border" size="sm" role="status">
+        <span className="visually-hidden">불러오는 중...</span>
+      </Spinner>
+    );
+  return (
+    <>
+      {data.map((theater) => {
+        return (
+          <Link key={theater.id} href={`/theaters/${theater.id}`} passHref>
+            <NavDropdown.Item>{theater.name}</NavDropdown.Item>
+          </Link>
+        );
+      })}
+    </>
+  );
+}

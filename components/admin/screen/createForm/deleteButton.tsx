718d8cc5c9261e38e115c4aa1a400def99263622
autroshot <autroshot@gamil.com>
2022-09-03 10:27:24 +0900

    구현: 관리자 상영관 등록의 인풋 삭제 버튼이 키보드로 탐색 가능하게 만듬

--- components/admin/screen/createForm/deleteButton.tsx
+++ components/admin/screen/createForm/deleteButton.tsx
@@ -2,7 +2,7 @@ import styles from './deleteButton.module.css';
 
 export default function DeleteButton() {
   return (
-    <span className="material-symbols-outlined fs-2" role="button">
+    <span className="material-symbols-outlined fs-2" role="button" tabIndex={0}>
       <span className={styles.color}>do_not_disturb_on</span>
     </span>
   );

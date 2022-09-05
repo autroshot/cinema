2856107a5a1c02674b1ce033f2a42251f28b3991
autroshot <autroshot@gamil.com>
2022-09-06 00:38:47 +0900

    구현: 좌석 배치도 CSS 변경. 선택 불가능한 좌석의 tabIndex 속성값 -1로 지정

--- components/admin/screen/createForm/seatButton.tsx
+++ components/admin/screen/createForm/seatButton.tsx
@@ -2,11 +2,14 @@ import styles from './seatButton.module.css';
 
 export default function seatButton(props: Props) {
   return (
-    <button className={styles[props.type]}>
-      {props.type !== 'unselectable' ? (
-        props.value
-      ) : (
+    <button
+      className={styles[props.type]}
+      tabIndex={props.type === 'unselectable' ? -1 : 0}
+    >
+      {props.type === 'unselectable' ? (
         <div className={styles.line} />
+      ) : (
+        props.value
       )}
     </button>
   );

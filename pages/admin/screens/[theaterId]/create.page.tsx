97b7c0e4d32524e69b7190a4439680517875f604
autroshot <autroshot@gamil.com>
2022-09-07 22:25:29 +0900

    구현: 관리자 상영관 등록의 등록 기능, 등록 완료 표시, 오류 표시

--- pages/admin/screens/[theaterId]/create.page.tsx
+++ pages/admin/screens/[theaterId]/create.page.tsx
@@ -14,9 +14,13 @@ import SeatingMap, {
 } from 'components/admin/screen/seatingMap/seatingMap';
 import { GetResponseData } from 'pages/api/theaters/[id].page';
 import axios from 'axios';
+import NoticeModal from 'components/admin/theater/noticeModal';
+import { ErrorResponseData } from 'pages/api/commonTypes';
 
 export default function CreateForm() {
   const [theaterName, setTheaterName] = useState<null | string>(null);
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState<null | string>(null);
 
   const {
@@ -38,6 +42,25 @@ export default function CreateForm() {
     mode: 'onChange',
     resolver: yupResolver(schema),
   });
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    console.log(data);
+
+    setLoading(true);
+    axios
+      .post('/api/screens', toRequestData(data))
+      .then(() => {
+        setShowModal(true);
+        setAlert(null);
+      })
+      .catch((err) => {
+        if (err.response) {
+          setAlert((err.response.data as ErrorResponseData).message);
+          return;
+        }
+        setAlert('오류');
+      })
+      .then(() => setLoading(false));
+  };
 
   const router = useRouter();
   const { theaterId } = router.query;
@@ -50,118 +73,116 @@ export default function CreateForm() {
       .catch((err) => setAlert(err));
   }, [theaterId]);
 
-  const onSubmit: SubmitHandler<FormInputs> = (data) => {
-    console.log(data);
-  };
   return (
-    <Container className="my-3">
-      <Form noValidate onSubmit={handleSubmit(onSubmit)}>
-        <h3 data-cy="title" className="mb-3">
-          상영관 등록
-        </h3>
-        <Row className="mb-3">
-          <Col>
-            <FloatingLabel controlId="screenNo" label="상영관 번호">
-              <Form.Control
-                type="number"
-                min="1"
-                placeholder="1"
-                {...register('no')}
-                isInvalid={Boolean(errors.no)}
+    <>
+      <Container className="my-3">
+        <Form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <h3 data-cy="title" className="mb-3">
+            상영관 등록
+          </h3>
+          <Row className="mb-3">
+            <Col>
+              <FloatingLabel controlId="screenNo" label="상영관 번호">
+                <Form.Control
+                  type="number"
+                  min="1"
+                  placeholder="1"
+                  {...register('no')}
+                  isInvalid={Boolean(errors.no)}
+                />
+                <Form.Control.Feedback type="invalid" className="fs-6">
+                  {errors.no?.message}
+                </Form.Control.Feedback>
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col>
+              <FloatingLabel
+                controlId="totalRow"
+                label="좌석 행 개수"
+                onChange={() => trigger(['aisles', 'unselectableSeats'])}
+              >
+                <Form.Control
+                  type="number"
+                  min="1"
+                  placeholder="1"
+                  {...register('totalRow')}
+                  isInvalid={Boolean(errors.totalRow)}
+                />
+                <Form.Control.Feedback type="invalid" className="fs-6">
+                  {errors.totalRow?.message}
+                </Form.Control.Feedback>
+              </FloatingLabel>
+            </Col>
+            <Col>
+              <FloatingLabel
+                controlId="totalColumn"
+                label="좌석 열 개수"
+                onChange={() => trigger(['aisles', 'unselectableSeats'])}
+              >
+                <Form.Control
+                  type="number"
+                  min="1"
+                  placeholder="1"
+                  {...register('totalColumn')}
+                  isInvalid={Boolean(errors.totalColumn)}
+                />
+                <Form.Control.Feedback type="invalid" className="fs-6">
+                  {errors.totalColumn?.message}
+                </Form.Control.Feedback>
+              </FloatingLabel>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col>
+              <h5>통로 만들기</h5>
+              <AisleInputs control={control} register={register} />
+            </Col>
+          </Row>
+          <Row className="mb-4">
+            <Col>
+              <h5>선택 불가능한 좌석 지정하기</h5>
+              <UnselectableSeatInputs
+                control={control}
+                register={register}
+                resetField={resetField}
               />
-              <Form.Control.Feedback type="invalid" className="fs-6">
-                {errors.no?.message}
-              </Form.Control.Feedback>
-            </FloatingLabel>
-          </Col>
-        </Row>
-
-        <Row className="mb-3">
-          <Col>
-            <FloatingLabel
-              controlId="totalRow"
-              label="좌석 행 개수"
-              onChange={() => trigger(['aisles', 'unselectableSeats'])}
-            >
-              <Form.Control
-                type="number"
-                min="1"
-                placeholder="1"
-                {...register('totalRow')}
-                isInvalid={Boolean(errors.totalRow)}
-              />
-              <Form.Control.Feedback type="invalid" className="fs-6">
-                {errors.totalRow?.message}
-              </Form.Control.Feedback>
-            </FloatingLabel>
-          </Col>
-
-          <Col>
-            <FloatingLabel
-              controlId="totalColumn"
-              label="좌석 열 개수"
-              onChange={() => trigger(['aisles', 'unselectableSeats'])}
-            >
-              <Form.Control
-                type="number"
-                min="1"
-                placeholder="1"
-                {...register('totalColumn')}
-                isInvalid={Boolean(errors.totalColumn)}
-              />
-              <Form.Control.Feedback type="invalid" className="fs-6">
-                {errors.totalColumn?.message}
-              </Form.Control.Feedback>
-            </FloatingLabel>
-          </Col>
-        </Row>
-
-        <Row className="mb-3">
-          <Col>
-            <h5>통로 만들기</h5>
-            <AisleInputs control={control} register={register} />
-          </Col>
-        </Row>
-
-        <Row className="mb-4">
-          <Col>
-            <h5>선택 불가능한 좌석 지정하기</h5>
-            <UnselectableSeatInputs
-              control={control}
-              register={register}
-              resetField={resetField}
-            />
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            {isValid ? (
-              <>
-                <h5 className="mb-3">좌석 배치도</h5>
-                <SeatingMap values={toSeatingMapValues(watch())} />
-                <p className="mt-3">
-                  {theaterName} 영화관에 해당 상영관을 등록합니다.
-                </p>
-              </>
-            ) : (
-              <p>모든 칸에 유효한 값을 입력하면 좌석 배치도가 표시됩니다.</p>
-            )}
-          </Col>
-        </Row>
-        {alert ? <MyAlert message={alert} /> : null}
-        <BottomButtons disabled={!isValid} loading={false} />
-      </Form>
-    </Container>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              {isValid ? (
+                <>
+                  <h5 className="mb-3">좌석 배치도</h5>
+                  <SeatingMap values={toSeatingMapValues(watch())} />
+                  <p className="mt-3">
+                    {theaterName} 영화관에 해당 상영관을 등록합니다.
+                  </p>
+                </>
+              ) : (
+                <p>모든 칸에 유효한 값을 입력하면 좌석 배치도가 표시됩니다.</p>
+              )}
+            </Col>
+          </Row>
+          {alert ? <MyAlert message={alert} /> : null}
+          <BottomButtons disabled={!isValid} loading={loading} />
+        </Form>
+      </Container>
+      <NoticeModal
+        show={showModal}
+        bodyText="등록이 완료되었습니다."
+        href="/admin/screens"
+        linkText="목록으로 돌아가기"
+        onClose={() => setShowModal(false)}
+      />
+    </>
   );
 
   function toSeatingMapValues(formInputs: FormInputs) {
     const result: Values = {
       totalRow: Number(formInputs.totalRow),
       totalColumn: Number(formInputs.totalColumn),
-      aisles: formInputs.aisles.map((aisle) => {
-        return { typeId: Number(aisle.typeId), no: Number(aisle.no) };
-      }),
       unselectableSeats: formInputs.unselectableSeats.map(
         (unselectableSeat) => {
           return {
@@ -171,9 +192,33 @@ export default function CreateForm() {
           };
         }
       ),
+      aisles: formInputs.aisles.map((aisle) => {
+        return { typeId: Number(aisle.typeId), no: Number(aisle.no) };
+      }),
     };
 
     return result;
+  }
+
+  function toRequestData(formInputs: FormInputs): PostRequestData {
+    return {
+      no: Number(formInputs.no),
+      total_row: Number(formInputs.totalRow),
+      total_column: Number(formInputs.totalColumn),
+      theater_id: Number(theaterId),
+      unselectable_seats: formInputs.unselectableSeats.map(
+        (unselectableSeat) => {
+          return {
+            unselectable_seat_type_id: Number(unselectableSeat.typeId),
+            row: Number(unselectableSeat.row),
+            column: Number(unselectableSeat.column),
+          };
+        }
+      ),
+      aisles: formInputs.aisles.map((aisle) => {
+        return { aisle_type_id: Number(aisle.typeId), no: Number(aisle.no) };
+      }),
+    };
   }
 }
 

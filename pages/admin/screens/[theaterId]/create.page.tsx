dde85c6793f00623e706dc9d73b82ac1ae94a38f
autroshot <autroshot@gamil.com>
2022-10-17 20:27:48 +0900

    리팩터링: 상영관 등록의 미사용 가져오기 제거

--- pages/admin/screens/[theaterId]/create.page.tsx
+++ pages/admin/screens/[theaterId]/create.page.tsx
@@ -30,7 +30,6 @@ export default function CreateForm({ unselectableSeatTypes }: Props) {
     watch,
     handleSubmit,
     trigger,
-    resetField,
     formState: { isValid, errors },
   } = useForm<FormInputs>({
     defaultValues: {

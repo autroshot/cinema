d989fac806830ce74fbcaf1392699329f5c68e9f
autroshot <autroshot@gamil.com>
2022-10-25 11:59:29 +0900

    리팩터링: 상영관 등록에서 공통 상영관 폼인 ScreenForm 컴포넌트 추출

---
+++ components/common/screenForm.tsx
@@ -0,0 +1,219 @@
+import { unselectable_seat_type } from '@prisma/client';
+import AisleInputs from 'components/admin/screen/createForm/aisleInputs';
+import NumberInputWithFloatingLabel from 'components/admin/screen/createForm/numberInputWithFloatingLabel';
+import UnselectableSeatInputs from 'components/admin/screen/createForm/unselectableSeatInputs';
+import SeatingMap from 'components/admin/screen/seatingMap/seatingMap';
+import MyAlert from 'components/admin/theater/myAlert';
+import { Row, Col } from 'react-bootstrap';
+import { useFormContext } from 'react-hook-form';
+
+export default function ScreenForm({
+  unselectableSeatTypes,
+  theaterName,
+  alert,
+}: Props) {
+  const {
+    control,
+    register,
+    watch,
+    trigger,
+    formState: { isValid, errors },
+  } = useFormContext<FormInputs>();
+
+  return (
+    <>
+      <Row className="mb-3">
+        <Col>
+          <NumberInputWithFloatingLabel<FormInputs>
+            controlId="screenNo"
+            label="상영관 번호"
+            min="1"
+            name="no"
+            register={register}
+            error={errors.no?.message}
+          />
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col>
+          <NumberInputWithFloatingLabel<FormInputs>
+            controlId="totalRow"
+            label="좌석 행 개수"
+            min="1"
+            name="totalRow"
+            register={register}
+            error={errors.totalRow?.message}
+            onChange={() => trigger(['aisles', 'unselectableSeats'])}
+          />
+        </Col>
+        <Col>
+          <NumberInputWithFloatingLabel<FormInputs>
+            controlId="totalColumn"
+            label="좌석 열 개수"
+            min="1"
+            name="totalColumn"
+            register={register}
+            error={errors.totalColumn?.message}
+            onChange={() => trigger(['aisles', 'unselectableSeats'])}
+          />
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col>
+          <h5>통로 만들기</h5>
+          <AisleInputs
+            control={control}
+            register={register}
+            trigger={trigger}
+          />
+        </Col>
+      </Row>
+      <Row className="mb-4">
+        <Col>
+          <h5>선택 불가능한 좌석 지정하기</h5>
+          <UnselectableSeatInputs
+            control={control}
+            register={register}
+            types={unselectableSeatTypes}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {isValid ? (
+            <>
+              <h5 className="mb-3">좌석 배치도</h5>
+              <SeatingMap values={toSeatingMapValues(watch())} />
+              <p className="mt-3">
+                {theaterName} 영화관에 해당 상영관을 등록합니다.
+              </p>
+            </>
+          ) : (
+            <p>모든 칸에 유효한 값을 입력하면 좌석 배치도가 표시됩니다.</p>
+          )}
+        </Col>
+      </Row>
+      {alert ? <MyAlert message={alert} /> : null}
+    </>
+  );
+
+  function toSeatingMapValues(formInputs: FormInputs) {
+    const result = convertFormInputs(formInputs);
+    result.aisles = removeOverlappingAndSortAisles(result.aisles);
+    result.unselectableSeats = removeOverlappingAndSortUnselectableSeats(
+      result.unselectableSeats
+    );
+
+    return result;
+  }
+}
+
+export function convertFormInputs(formInputs: FormInputs): ConvertedFormInputs {
+  return {
+    no: Number(formInputs.no),
+    totalRow: Number(formInputs.totalRow),
+    totalColumn: Number(formInputs.totalColumn),
+    unselectableSeats: formInputs.unselectableSeats.map((unselectableSeat) => {
+      return {
+        typeId: Number(unselectableSeat.typeId),
+        row: Number(unselectableSeat.row),
+        column: Number(unselectableSeat.column),
+      };
+    }),
+    aisles: formInputs.aisles.map((aisle) => {
+      return { typeId: Number(aisle.typeId), no: Number(aisle.no) };
+    }),
+  };
+}
+
+export function removeOverlappingAndSortAisles(aisles: aisles): aisles {
+  const rowAisles = aisles.filter((aisle) => aisle.typeId === 1);
+  const columnAisles = aisles.filter((aisle) => aisle.typeId === 2);
+
+  const resultRowAisles = [...new Set(rowAisles.map((aisle) => aisle.no))]
+    .sort((a, b) => a - b)
+    .map((aisleNumber) => {
+      return { typeId: 1, no: aisleNumber };
+    });
+  const resultColumnAisles = [...new Set(columnAisles.map((aisle) => aisle.no))]
+    .sort((a, b) => a - b)
+    .map((aisleNumber) => {
+      return { typeId: 2, no: aisleNumber };
+    });
+
+  return [...resultRowAisles, ...resultColumnAisles];
+}
+
+export function removeOverlappingAndSortUnselectableSeats(
+  unselectableSeats: unselectableSeats
+): unselectableSeats {
+  return sortUnselectableSeats(
+    removeOverlappingUnselectableSeats(unselectableSeats)
+  );
+}
+
+function removeOverlappingUnselectableSeats(
+  unselectableSeats: unselectableSeats
+): unselectableSeats {
+  const result: unselectableSeats = [];
+
+  unselectableSeats.forEach((unselectableSeat) => {
+    const foundIndex = result.findIndex((element) => {
+      return (
+        element.row === unselectableSeat.row &&
+        element.column === unselectableSeat.column
+      );
+    });
+
+    if (foundIndex === -1) return result.push(unselectableSeat);
+    if (unselectableSeat.typeId < unselectableSeats[foundIndex].typeId)
+      return (unselectableSeats[foundIndex].typeId = unselectableSeat.typeId);
+  });
+
+  return result;
+}
+
+function sortUnselectableSeats(
+  unselectableSeats: unselectableSeats
+): unselectableSeats {
+  return unselectableSeats.sort((seatA, seatB) => {
+    if (seatA.row > seatB.row) return 1;
+    if (seatA.row < seatB.row) return -1;
+    if (seatA.column > seatB.column) return 1;
+    if (seatA.column < seatB.column) return -1;
+    return 0;
+  });
+}
+
+export interface FormInputs {
+  no: string | null;
+  totalRow: string | null;
+  totalColumn: string | null;
+  aisles: { typeId: string; no: string | null }[];
+  unselectableSeats: {
+    typeId: string;
+    row: string | null;
+    column: string | null;
+  }[];
+}
+
+interface ConvertedFormInputs {
+  no: number;
+  totalRow: number;
+  totalColumn: number;
+  aisles: { typeId: number; no: number }[];
+  unselectableSeats: {
+    typeId: number;
+    row: number;
+    column: number;
+  }[];
+}
+type unselectableSeats = ConvertedFormInputs['unselectableSeats'];
+type aisles = ConvertedFormInputs['aisles'];
+
+export type UnselectableSeatTypes = unselectable_seat_type[];
+interface Props {
+  unselectableSeatTypes: UnselectableSeatTypes;
+  theaterName: null | string;
+  alert: null | string;
+}
--- pages/admin/screens/[theaterId]/create.page.tsx
+++ pages/admin/screens/[theaterId]/create.page.tsx
@@ -1,15 +1,11 @@
-import AisleInputs from 'components/admin/screen/createForm/aisleInputs';
-import UnselectableSeatInputs from 'components/admin/screen/createForm/unselectableSeatInputs';
-import MyAlert from 'components/admin/theater/myAlert';
 import { useRouter } from 'next/router';
 import { PostRequestData } from 'pages/api/screens/index.page';
 import { useEffect, useState } from 'react';
-import { Col, Container, Form, Row } from 'react-bootstrap';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Container, Form } from 'react-bootstrap';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import BottomButtons from 'components/admin/screen/createForm/bottomButtons';
 import { schema } from './create.yup';
-import SeatingMap from 'components/admin/screen/seatingMap/seatingMap';
 import { GetResponseData } from 'pages/api/theaters/[id].page';
 import axios from 'axios';
 import NoticeModal from 'components/admin/theater/noticeModal';
@@ -17,7 +13,12 @@ import { ErrorResponseData } from 'pages/api/commonTypes';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { prisma } from 'db';
 import { unselectable_seat_type } from '@prisma/client';
-import NumberInputWithFloatingLabel from 'components/admin/screen/createForm/numberInputWithFloatingLabel';
+import ScreenForm, {
+  convertFormInputs,
+  FormInputs,
+  removeOverlappingAndSortAisles,
+  removeOverlappingAndSortUnselectableSeats,
+} from 'components/common/screenForm';
 
 export default function CreateForm({ unselectableSeatTypes }: Props) {
   const [theaterName, setTheaterName] = useState<null | string>(null);
@@ -25,14 +26,7 @@ export default function CreateForm({ unselectableSeatTypes }: Props) {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState<null | string>(null);
 
-  const {
-    control,
-    register,
-    watch,
-    handleSubmit,
-    trigger,
-    formState: { isValid, errors },
-  } = useForm<FormInputs>({
+  const methods = useForm<FormInputs>({
     defaultValues: {
       no: null,
       totalRow: null,
@@ -43,6 +37,10 @@ export default function CreateForm({ unselectableSeatTypes }: Props) {
     mode: 'onChange',
     resolver: yupResolver(schema),
   });
+  const {
+    handleSubmit,
+    formState: { isValid },
+  } = methods;
 
   const onSubmit: SubmitHandler<FormInputs> = async (data) => {
     // TODO: 테스트용
@@ -90,78 +88,13 @@ export default function CreateForm({ unselectableSeatTypes }: Props) {
           <h3 data-cy="title" className="mb-3">
             상영관 등록
           </h3>
-          <Row className="mb-3">
-            <Col>
-              <NumberInputWithFloatingLabel<FormInputs>
-                controlId="screenNo"
-                label="상영관 번호"
-                min="1"
-                name="no"
-                register={register}
-                error={errors.no?.message}
-              />
-            </Col>
-          </Row>
-          <Row className="mb-3">
-            <Col>
-              <NumberInputWithFloatingLabel<FormInputs>
-                controlId="totalRow"
-                label="좌석 행 개수"
-                min="1"
-                name="totalRow"
-                register={register}
-                error={errors.totalRow?.message}
-                onChange={() => trigger(['aisles', 'unselectableSeats'])}
-              />
-            </Col>
-            <Col>
-              <NumberInputWithFloatingLabel<FormInputs>
-                controlId="totalColumn"
-                label="좌석 열 개수"
-                min="1"
-                name="totalColumn"
-                register={register}
-                error={errors.totalColumn?.message}
-                onChange={() => trigger(['aisles', 'unselectableSeats'])}
-              />
-            </Col>
-          </Row>
-          <Row className="mb-3">
-            <Col>
-              <h5>통로 만들기</h5>
-              <AisleInputs
-                control={control}
-                register={register}
-                trigger={trigger}
-              />
-            </Col>
-          </Row>
-          <Row className="mb-4">
-            <Col>
-              <h5>선택 불가능한 좌석 지정하기</h5>
-              <UnselectableSeatInputs
-                control={control}
-                register={register}
-                types={unselectableSeatTypes}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              {isValid ? (
-                <>
-                  <h5 className="mb-3">좌석 배치도</h5>
-                  <SeatingMap values={toSeatingMapValues(watch())} />
-                  <p className="mt-3">
-                    {theaterName} 영화관에 해당 상영관을 등록합니다.
-                  </p>
-                </>
-              ) : (
-                <p>모든 칸에 유효한 값을 입력하면 좌석 배치도가 표시됩니다.</p>
-              )}
-            </Col>
-          </Row>
-          {alert ? <MyAlert message={alert} /> : null}
+          <FormProvider {...methods}>
+            <ScreenForm
+              unselectableSeatTypes={unselectableSeatTypes}
+              theaterName={theaterName}
+              alert={alert}
+            />
+          </FormProvider>
           <BottomButtons disabled={!isValid} loading={loading} />
         </Form>
       </Container>
@@ -174,16 +107,6 @@ export default function CreateForm({ unselectableSeatTypes }: Props) {
       />
     </>
   );
-
-  function toSeatingMapValues(formInputs: FormInputs) {
-    const result = convertFormInputs(formInputs);
-    result.aisles = removeOverlappingAndSortAisles(result.aisles);
-    result.unselectableSeats = removeOverlappingAndSortUnselectableSeats(
-      result.unselectableSeats
-    );
-
-    return result;
-  }
 
   function toRequestData(formInputs: FormInputs): PostRequestData {
     const convertedFormInputs = convertFormInputs(formInputs);
@@ -213,87 +136,6 @@ export default function CreateForm({ unselectableSeatTypes }: Props) {
       }),
     };
   }
-
-  function convertFormInputs(formInputs: FormInputs): ConvertedFormInputs {
-    return {
-      no: Number(formInputs.no),
-      totalRow: Number(formInputs.totalRow),
-      totalColumn: Number(formInputs.totalColumn),
-      unselectableSeats: formInputs.unselectableSeats.map(
-        (unselectableSeat) => {
-          return {
-            typeId: Number(unselectableSeat.typeId),
-            row: Number(unselectableSeat.row),
-            column: Number(unselectableSeat.column),
-          };
-        }
-      ),
-      aisles: formInputs.aisles.map((aisle) => {
-        return { typeId: Number(aisle.typeId), no: Number(aisle.no) };
-      }),
-    };
-  }
-
-  function removeOverlappingAndSortAisles(aisles: aisles): aisles {
-    const rowAisles = aisles.filter((aisle) => aisle.typeId === 1);
-    const columnAisles = aisles.filter((aisle) => aisle.typeId === 2);
-
-    const resultRowAisles = [...new Set(rowAisles.map((aisle) => aisle.no))]
-      .sort((a, b) => a - b)
-      .map((aisleNumber) => {
-        return { typeId: 1, no: aisleNumber };
-      });
-    const resultColumnAisles = [
-      ...new Set(columnAisles.map((aisle) => aisle.no)),
-    ]
-      .sort((a, b) => a - b)
-      .map((aisleNumber) => {
-        return { typeId: 2, no: aisleNumber };
-      });
-
-    return [...resultRowAisles, ...resultColumnAisles];
-  }
-
-  function removeOverlappingAndSortUnselectableSeats(
-    unselectableSeats: unselectableSeats
-  ): unselectableSeats {
-    return sortUnselectableSeats(
-      removeOverlappingUnselectableSeats(unselectableSeats)
-    );
-  }
-
-  function removeOverlappingUnselectableSeats(
-    unselectableSeats: unselectableSeats
-  ): unselectableSeats {
-    const result: unselectableSeats = [];
-
-    unselectableSeats.forEach((unselectableSeat) => {
-      const foundIndex = result.findIndex((element) => {
-        return (
-          element.row === unselectableSeat.row &&
-          element.column === unselectableSeat.column
-        );
-      });
-
-      if (foundIndex === -1) return result.push(unselectableSeat);
-      if (unselectableSeat.typeId < unselectableSeats[foundIndex].typeId)
-        return (unselectableSeats[foundIndex].typeId = unselectableSeat.typeId);
-    });
-
-    return result;
-  }
-
-  function sortUnselectableSeats(
-    unselectableSeats: unselectableSeats
-  ): unselectableSeats {
-    return unselectableSeats.sort((seatA, seatB) => {
-      if (seatA.row > seatB.row) return 1;
-      if (seatA.row < seatB.row) return -1;
-      if (seatA.column > seatB.column) return 1;
-      if (seatA.column < seatB.column) return -1;
-      return 0;
-    });
-  }
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -319,32 +161,6 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   };
 };
-
-export interface FormInputs {
-  no: string | null;
-  totalRow: string | null;
-  totalColumn: string | null;
-  aisles: { typeId: string; no: string | null }[];
-  unselectableSeats: {
-    typeId: string;
-    row: string | null;
-    column: string | null;
-  }[];
-}
-
-interface ConvertedFormInputs {
-  no: number;
-  totalRow: number;
-  totalColumn: number;
-  aisles: { typeId: number; no: number }[];
-  unselectableSeats: {
-    typeId: number;
-    row: number;
-    column: number;
-  }[];
-}
-type unselectableSeats = ConvertedFormInputs['unselectableSeats'];
-type aisles = ConvertedFormInputs['aisles'];
 
 export type UnselectableSeatTypes = unselectable_seat_type[];
 interface Props {

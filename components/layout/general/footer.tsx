1876adafa0179678c4239373560b8baefef3b8a2
autroshot <autroshot@gamil.com>
2022-11-22 15:21:46 +0900

    구현: 푸터에 깃허브 아이콘

--- components/layout/general/footer.tsx
+++ components/layout/general/footer.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import logo from 'public/logos/site-with-text-gray.png';
 import { Container } from 'react-bootstrap';
+import styles from './footer.module.css';
 
 export default function Footer() {
   return (
@@ -10,7 +11,12 @@ export default function Footer() {
           <Image src={logo} alt="시네마 로고" />
         </div>
         <div>
-          <a href="https://github.com/autroshot/cinema">GitHub 링크</a>
+          <a
+            href="https://github.com/autroshot/cinema"
+            target="_blank"
+            rel="noreferrer"
+            className={styles.gitHubIcon}
+          />
         </div>
       </footer>
     </Container>

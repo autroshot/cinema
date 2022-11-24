ba223c6ef8eebf91bda7fc0c8978088411f1198b
autroshot <autroshot@gamil.com>
2022-11-25 00:13:28 +0900

    리팩터링: TODO 제거

--- components/theater/movieRecommendation.tsx
+++ components/theater/movieRecommendation.tsx
@@ -10,25 +10,20 @@ export default function MovieRecommendation(props: Props) {
       <Row>
         <Col className="col">
           <div>
-            {/* TODO: 미구현 */}
             autroshot님
             <br />
             <b>{props.theaterName}</b>에서
             <br />
-            {/* TODO: 미구현 */}
             <span className={styles.movieTitle}>한산: 용의 출현</span>
             <br />
             어떠세요?
           </div>
           <hr />
           <div>
-            {/* TODO: 미구현 */}
             한산: 용의 출현
             <br />
-            {/* TODO: 미구현 */}
             예매율 33.5%{' '}
             <span className="material-symbols-outlined">
-              {/* TODO: 미구현 */}
               <span className={styles.grade}>grade</span>
             </span>{' '}
             9.4
@@ -37,10 +32,8 @@ export default function MovieRecommendation(props: Props) {
           <div>
             <ShareURL />
             <span className="material-symbols-outlined me-3" role="button">
-              {/* TODO: 미구현 */}
               <span className={styles.favorite}>favorite</span>
             </span>
-            {/* TODO: 미구현 */}
             <Button className="btn-sm btn-primary">예매하기</Button>
           </div>
         </Col>

bfeaf080c564be8ec370ee8079ca729844964a90
autroshot <autroshot@gamil.com>
2022-11-25 18:30:42 +0900

    리팩터링: 관리자 레이아웃에서 네비바 컴포넌트 추출

--- components/layout/admin/layout.tsx
+++ components/layout/admin/layout.tsx
@@ -1,38 +1,12 @@
-import Link from 'next/link';
 import { ScriptProps } from 'next/script';
-import { Container, Nav } from 'react-bootstrap';
-import { signOut } from 'next-auth/react';
+import { Container } from 'react-bootstrap';
 import Footer from '../common/footer';
+import Navbar from './navbar';
 
 export default function Layout({ children }: ScriptProps) {
   return (
     <Container>
-      <Nav className="bg-light" as="ul">
-        <Nav.Item as="li">
-          <Nav.Link as="div">
-            <Link href="/admin/">홈</Link>
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link as="div">
-            <Link href="/admin/theaters">영화관</Link>
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link as="div">
-            <Link href="/admin/screens">상영관</Link>
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item
-          as="li"
-          className="ms-0 ms-sm-auto"
-          onClick={() => signOut({ callbackUrl: '/' })}
-        >
-          <Nav.Link as="div">
-            <a>관리자 나가기</a>
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <Navbar />
       <main>{children}</main>
       <Footer />
     </Container>
---
+++ components/layout/admin/navbar.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import { Nav } from 'react-bootstrap';
+import { signOut } from 'next-auth/react';
+
+export default function Navbar() {
+  return (
+    <Nav className="bg-light" as="ul">
+      <Nav.Item as="li">
+        <Nav.Link as="div">
+          <Link href="/admin/">홈</Link>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link as="div">
+          <Link href="/admin/theaters">영화관</Link>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link as="div">
+          <Link href="/admin/screens">상영관</Link>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item
+        as="li"
+        className="ms-0 ms-sm-auto"
+        onClick={() => signOut({ callbackUrl: '/' })}
+      >
+        <Nav.Link as="div">
+          <a>관리자 나가기</a>
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}

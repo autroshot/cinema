869e509119b60db6a1b018d3765f490345490127
autroshot <autroshot@gamil.com>
2022-12-04 23:54:34 +0900

    구현: 상영관의 통로와 좌석 삭제 버튼의 CSS 변경

--- components/admin/screen/createForm/deleteButton.tsx
+++ components/admin/screen/createForm/deleteButton.tsx
@@ -3,7 +3,7 @@ import styles from './deleteButton.module.css';
 export default function DeleteButton() {
   return (
     <span className="material-symbols-outlined fs-2">
-      <span className={styles.color} role="button" tabIndex={0}>
+      <span className={styles.button} role="button" tabIndex={0}>
         do_not_disturb_on
       </span>
     </span>
